3ab46a00cb19950b59512ba8312c6c13aa303087
Greg Annandale <greg@gregannandale.com>
2026-03-22 22:16:47 +0000

    Bump version to 0.18.5

--- site/data.js
+++ site/data.js
@@ -1438,4 +1438,4 @@ Interactive controls:
 Aliases: "install" = "setup" = "i", "run" = "start" = "r", "test" = "t",
          "deploy" = "d", "check" = "c"`;
 
-const VERSION = "0.18.4";
+const VERSION = "0.18.5";
